7036c39f2815ad0737feeb8da7f257bd23b0ee60
Heine Günther <heine_gunther@hotmail.com>
2024-09-23 16:17:33 +0200

    Added a paragraph to the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hei Fra Ernæringskokken</h1>
+      <h1>Hei Fra Ernæringskokken!!</h1>
+      <p>Hei fra Ernæringskokken!!</p>
     </div>
   );
 }
