2f34ac46e9beec2610ef5529ef585f0e9c18f7d3
Heine Günther <heine_gunther@hotmail.com>
2024-09-24 09:17:20 +0200

    installed some tools

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,18 @@ import { motion } from "framer-motion";
 function App() {
   return (
     <motion.div
-      className="flex items-center justify-center min-h-screen bg-gray-100"
+      className="flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-4"
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.5 }}
     >
-      <h1 className="text-3xl font-bold text-red-600">
+      <h1 className="text-5xl font-extrabold text-red-700">
         Hei Fra Ernæringskokken!!
       </h1>
-      <h2 className="text-3xl font-bold text-blue-600">
-        Det ser ut til å fungere
+      <h2 className="text-4xl font-bold text-green-600">
+        Det ser ut til å fungere
       </h2>
-      <p className="text-3xl font-bold text-blue-600">
-        Hei igjen fra Ernæringskokken!!
-      </p>
+      <p className="text-xl text-gray-800">Hei igjen fra Ernæringskokken!!</p>
     </motion.div>
   );
 }
